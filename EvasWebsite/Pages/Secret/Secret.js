8277875be1c1af01b4e8657422a024461f67709c
Markus Reynolds <markusreynolds1989@gmail.com>
2019-09-02 10:11:07 -0500

    Start on secret project.

---
+++ EvasWebsite/Pages/Secret/Secret.js
@@ -0,0 +1,11 @@
+﻿/* A fun game 
+ * for a break on this crazy
+ * huge project
+ */
+
+const Screen = document.getElementById("Game");
+const ctx = Screen.getContext('2d');
+ctx.moveTo(100, 100);
+ctx.lineTo(50, 50);
+ctx.stroke();
+
