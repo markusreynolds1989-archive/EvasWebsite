1dc1aaa9ca0b5a7e2f7cd6f2b7aef49cdb17dcdd
Markus Reynolds <markusreynolds1989@gmail.com>
2019-09-02 17:55:53 -0500

    JS for secret game

--- EvasWebsite/Pages/Secret/Secret.js
+++ EvasWebsite/Pages/Secret/Secret.js
@@ -5,13 +5,38 @@
 
 const Screen = document.getElementById("Game");
 const ctx = Screen.getContext('2d');
+let score = "Score: " + 0;
 
-const head = (x, y) =>{
+/* here's the head and it's current position */
+const head = (x, y) =>
+{
     ctx.rect(x, y, 10, 10);
     ctx.fill();
 }
 
-const load = () => { head(250, 490); ctx.stroke(); }
+/* draw the score on the top right */
+let putScore = (score) =>
+{
+    ctx.font = '24px serif';
+    ctx.fillText(score, 370, 30);
+};
 
+/* what happens on screen on load */
+const load = () =>
+{
+    head(250, 490);
+    putScore(score);
+    ctx.stroke();
+}
+
+/*char => int => int*/
+/*takes a key press and returns an integer or logic to add */
+const eventHandler = (e) => { return null;}
+
+/*should be the update */
+/*nothing => side effects*/
+const gameUpdate = () => { return "TEST";}
 
 load();
+
+gameUpdate();
