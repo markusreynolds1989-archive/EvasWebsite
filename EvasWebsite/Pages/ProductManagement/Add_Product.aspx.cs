a133f8bc9e10d6c752d630b96a8f84d9b5aeb0c5
Markus Reynolds <markus.reynolds1989@gmail.com>
2019-09-03 08:19:48 -0500

    Fixed cost/quantity

--- EvasWebsite/Pages/ProductManagement/Add_Product.aspx.cs
+++ EvasWebsite/Pages/ProductManagement/Add_Product.aspx.cs
@@ -81,7 +81,7 @@ namespace EvasWebsite.Pages
                 product.Title = txtTitle.Text;
                 product.Description = txtDescription.Text;
                 product.Quantity = Convert.ToInt32(txtQuantity.Text);
-                product.Cost = Convert.ToSingle(txtQuantity.Text);
+                product.Cost = Convert.ToSingle(txtCost.Text);
                 if (upPicture.HasFile)
                 {
                     product.PicturePath = "~/Pictures/" + upPicture.FileName;
