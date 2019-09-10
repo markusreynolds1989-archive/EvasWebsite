1d238b0d416d83707d3ccd00b71a988626b378a9
Markus Reynolds <markus.reynolds1989@gmail.com>
2019-09-10 14:26:11 -0500

    Added in tests for datalayer, more to come.

---
+++ EvasWebsite/Tests/DatabaseTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using EvasWebsite.Data;
+
+namespace EvasWebsite.Tests
+{
+    [TestClass]
+    public class DatabaseTests
+    {
+        [TestMethod]
+        public void addProductTest()
+        {
+            Assert.IsTrue(Data_Layer.AddProduct("test", "test", 2, 2, "test"));
+        }
+
+        [TestMethod]
+        public void addUserTest()
+        {
+            Assert.IsTrue(Data_Layer.AddUser("test", "test", 0));
+        }
+    }
+}
